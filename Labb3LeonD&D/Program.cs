afa0787d9a10cd939aca5d3eefdfe184b784e971
Conix <Conix@DESKTOP-CNLH10G>
2025-01-29 23:33:46 +0100

    Add project files.

---
+++ Labb3LeonD&D/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb3LeonD_D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
