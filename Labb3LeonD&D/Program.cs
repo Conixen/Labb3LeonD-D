c5ad686212d7e769dacc6869ef75d895ed2be126
Conixen <leon.json57@gmail.com>
2025-02-02 00:10:40 +0100

    show alla studets from 1 class

--- Labb3LeonD&D/Program.cs
+++ Labb3LeonD&D/Program.cs
@@ -45,31 +45,80 @@ namespace Labb3LeonD_D
 
             // Fetching all students from 1 class
 
-            //using (SqlConnection connection = new SqlConnection(connectionString2)) 
-            //{ 
-            //    connection.Open();
+            using (SqlConnection connection = new SqlConnection(connectionString2))
+            {
+                connection.Open();
 
-            //    string queryClass = "SELECT ClassID FROM STUDENT ";
-            //    connection.Close();
-            //}
+                string queryClass = "SELECT ClassID, ClassName FROM Class ";
+                SqlCommand commandClass = new SqlCommand(queryClass, connection);
+
+                SqlDataReader reader = commandClass.ExecuteReader();
+
+                List<(int ID, string Name)> allClasses = new List<(int ID, string Name)> ();
+
+                Console.WriteLine("Välj en klass:");
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0); 
+                    string name = reader.GetString(1); 
+
+                    allClasses.Add((id, name));
+                    Console.WriteLine($"{id}: {name}");
+                }
+                reader.Close();
+                Console.Write("Välj vilken klass: ");
+                if (int.TryParse(Console.ReadLine(), out int classId))
+                {
+                    // Hämta elever i vald klass
+                    string queryStudents = "SELECT FirstName, LastName FROM Student WHERE ClassID = @ClassId";
+                    SqlCommand cmdStudents = new SqlCommand(queryStudents, connection);
+                    cmdStudents.Parameters.AddWithValue("@ClassId", classId);
+
+                    SqlDataReader readerStudents = cmdStudents.ExecuteReader();
+
+                    Console.WriteLine("\nElever i vald klass:");
+                    bool foundStudents = false;
+
+                    while (readerStudents.Read())
+                    {
+                        foundStudents = true;
+                        Console.WriteLine($"- {readerStudents.GetString(0)} {readerStudents.GetString(1)}");
+                    }
+
+                    if (!foundStudents)
+                    {
+                        Console.WriteLine("Inga elever hittades i denna klass.");
+                    }
+
+                    readerStudents.Close();
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt klass-ID.");
+                }
+
+                connection.Close();
+            }
+
 
             // Add more employe
 
-            using (SqlConnection connection = new SqlConnection(connectionString2)) 
-            {
-                string queryAdd = @"INSERT INTO EMPLOYE (EmployeName, RollID) 
-                                    VALUES (@EmployeName, @RollID)";
+            //using (SqlConnection connection = new SqlConnection(connectionString2)) 
+            //{
+            //    string queryAdd = @"INSERT INTO EMPLOYE (EmployeName, RollID) 
+            //                        VALUES (@EmployeName, @RollID)";
 
-                using (SqlCommand commandAdd = new SqlCommand(queryAdd, connection)) 
-                {
-                    commandAdd.Parameters.AddWithValue("@EmployeName", "Bo Boström");
-                    commandAdd.Parameters.AddWithValue("@RollID", "5");
+            //    using (SqlCommand commandAdd = new SqlCommand(queryAdd, connection)) 
+            //    {
+            //        commandAdd.Parameters.AddWithValue("@EmployeName", "Bo Boström");
+            //        commandAdd.Parameters.AddWithValue("@RollID", "5");
 
-                    connection.Open();
-                    var rowsAffected = commandAdd.ExecuteNonQuery();
-                    Console.WriteLine(rowsAffected);
-                }
-            }
+            //        connection.Open();
+            //        var rowsAffected = commandAdd.ExecuteNonQuery();
+            //        Console.WriteLine(rowsAffected);
+            //    }
+            //}
+            // Fix comand thats shows the new added employe
         }
     }
 }
